d57f361ab2e47c0f23defd58670dd3073d65676a
Wahyu Candra <wcandra01@gmail.com>
2020-10-14 21:40:50 +0700

    tugas praktikum

---
+++ data_mhs.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+using namespace std;
+
+int main () {
+    char nama[36], nim[12];
+    int uts, uas, avg;
+
+    cout<<"Nama\t: ";
+    cin>>nama;
+    cout<<"NIM\t: ";
+    cin>>nim;
+    cout<<"UTS\t: ";
+    cin>>uts;
+    cout<<"UAS\t: ";
+    cin>>uas;
+
+    avg = (uts+uas)/2;
+
+    printf("\nHai, nama saya %s, nim %s \nNilai akhir saya %d\n\n", nama, nim, avg);
+
+    system("pause");
+}
