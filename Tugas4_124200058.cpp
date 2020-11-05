e3fe4aaf1ecf5a0b478ffd9f46603e000cb6ad20
Wahyu Candra <wcandra01@gmail.com>
2020-11-05 16:51:37 +0700

    Tugas 4 - Revisi

--- Tugas4_124200058.cpp
+++ Tugas4_124200058.cpp
@@ -8,19 +8,8 @@ int main () {
     cout<<"Perkalian & Perpangkatan"<<setfill('=')<<setw(6)<<'='<<endl;
     cout<<"1. Perkalian\n"<<"2. Perpangkatan\n"<<"Pilih : ";
     cin>>type;
-    
-    switch (type) {
-        case 1:
-            type = 1;
-            break;
-        case 2:
-            type = 2;
-            break;
-        default:
-            type = 0;
-    }
 
-    if (type == 0) {
+    if (type != 1 && type != 2) {
         cout<<"salah input!";
     } else {
         cout<<"Input angka N : ";
@@ -29,32 +18,34 @@ int main () {
         cin>>m;
         cout<<"Hasil ";
         
-        if (type == 1) {
-            cout<<n<<" x "<<m<<endl;
-            kali = 0;
+        switch (type) {
+            case 1:
+                cout<<n<<" x "<<m<<endl;
 
-            for (int x = 1; x <= n; x++) {
-                kali += m;
+                for (int x = 1; x <= n; x++) {
+                    kali += m;
 
-                if (x != n) {
-                    cout<<m<<" + ";
-                } else {
-                    cout<<m<<" = "<<kali;
+                    if (x != n) {
+                        cout<<m<<" + ";
+                    } else {
+                        cout<<m<<" = "<<kali;
+                    }
                 }
-            }
-        } else {
-            cout<<n<<" ^ "<<m<<endl;
-            pangkat = 1; 
-            
-            for (int x = 1; x <= m; x++) {
-                pangkat *= n;
+                break;
+            case 2:
+                cout<<n<<" ^ "<<m<<endl;
+                pangkat = 1; 
+                
+                for (int x = 1; x <= m; x++) {
+                    pangkat *= n;
 
-                if (x != m ) {
-                    cout<<n<<" x ";
-                } else {
-                    cout<<n<<" = "<<pangkat;
+                    if (x != m ) {
+                        cout<<n<<" x ";
+                    } else {
+                        cout<<n<<" = "<<pangkat;
+                    }
                 }
-            }
+                break;
         }
     }
 
