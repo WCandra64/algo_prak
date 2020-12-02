22da2975dca60add4b0f8bd5b3f40fe862660de1
Wahyu Candra <wcandra01@gmail.com>
2020-12-02 20:12:38 +0700

    Tugas 5 - rev

--- Tugas5_124200058.cpp
+++ Tugas5_124200058.cpp
@@ -1,9 +1,22 @@
+/*  PSEUDOCODE 
+    Declaration :
+        type, p, q, x, y : integer;
+    
+    Description :
+        input(type)
+        case(type)
+            1 : input(y)
+                input(x)
+                
+
+*/
+
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
 int main () {
-    int type, p, q, x, y;
+    int type, p, q, x, y, z;
 
     cout<<"Gambar Bangun Datar "<<setfill('=')<<setw(11)<<'='<<endl;
     cout<<"1. Kotak Bolong\n"<<"2. Segitiga\n"<<"Pilih : ";
@@ -34,14 +47,14 @@ int main () {
             break;
         case 2:
             cout<<"Tinggi\t: ";
-            cin>>y;
+            cin>>z;
 
-            for (p = 1; p <= y; p++) {
-                x=1;
-                cout<<p;
-                for (q = 1; q < p; q++) {
-                    cout<<" "<<p+(y*q)-x;
-                    x=x+q+1;
+            for (p = 1; p <= z; p++) {
+                x=p;
+                y=z;
+                for (q = 0; q < p; q++) {
+                    cout<<x<<" ";
+                    x=--y+x;
                 }
                 cout<<endl;
             }
